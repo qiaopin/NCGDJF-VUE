5418768ef1d9f47614e5f4dd59047f25c7c14a27
fancheng231 <605892980@qq.com>
2021-03-09 09:14:03 +0800

    123

--- src/views/login/components/showDialog.vue
+++ src/views/login/components/showDialog.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="alertD">
     <div><span style="color: red">当前app仅支持安卓手机</span></div>
-    <div>
-      打开手机浏览器输入http://110.249.159.46:8099/download/app-release.apk
-    </div>
+    <div>打开手机浏览器输入{{ downLoadUrl }}</div>
     <div>
       (或者使用手机浏览器扫描下方二维码(<span style="color: red"
         >不支持直接使用微信扫码</span
@@ -13,17 +11,20 @@
       <img src="@/assets/ncgdjfAppSJZ.png" width="200" height="200" alt="" />
     </div>
     <div>
-      或者先<a
-        style="color: blue"
-        href="http://110.249.159.46:8099/download/app-release.apk"
-        >下载到电脑端</a
+      或者先<a style="color: blue" :href="downLoadUrl">下载到电脑端</a
       >再发送到手机进行安装
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      downLoadUrl: 'http://110.249.159.162:9990/download/app-release.apk',
+    }
+  },
+}
 </script>
 
 <style>
